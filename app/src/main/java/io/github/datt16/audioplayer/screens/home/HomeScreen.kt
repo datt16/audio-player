a5ebe9d620d74535ffea9ecb4e058bd6e4475e5c
datt16 <osatom.4639@gmail.com>
2025-03-27 19:32:55 +0900

    fmt

--- app/src/main/java/io/github/datt16/audioplayer/screens/home/HomeScreen.kt
+++ app/src/main/java/io/github/datt16/audioplayer/screens/home/HomeScreen.kt
@@ -1,25 +1,16 @@
 package io.github.datt16.audioplayer.screens.home
 
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import androidx.media3.common.util.UnstableApi
-import androidx.media3.ui.compose.PlayerSurface
-import androidx.media3.ui.compose.SURFACE_TYPE_TEXTURE_VIEW
 import io.github.datt16.audioplayer.core.designsystem.AudioPlayerAppTheme
-import io.github.datt16.audioplayer.core.ext.black
 import io.github.datt16.audioplayer.viewmodels.HomeViewModel
 
 @Composable
@@ -27,18 +18,15 @@ fun HomeScreen(
   modifier: Modifier = Modifier,
   viewModel: HomeViewModel = hiltViewModel(),
 ) {
-  val player = viewModel.getExoPlayer()
-
   LaunchedEffect(Unit) {
     viewModel.startPlayback("https://storage.googleapis.com/exoplayer-test-media-0/play.mp3")
   }
 
   Column(
     modifier = modifier
-        .fillMaxWidth()
-        .padding(horizontal = 16.dp)
+      .fillMaxWidth()
+      .padding(horizontal = 16.dp)
   ) {
-
   }
 }
 
--- app/src/main/java/io/github/datt16/audioplayer/viewmodels/HomeViewModel.kt
+++ app/src/main/java/io/github/datt16/audioplayer/viewmodels/HomeViewModel.kt
@@ -2,24 +2,13 @@ package io.github.datt16.audioplayer.viewmodels
 
 import androidx.core.net.toUri
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
-import androidx.media3.common.util.UnstableApi
 import dagger.hilt.android.lifecycle.HiltViewModel
 import io.github.datt16.audioplayer.core.player.PlaybackManager
-import io.github.datt16.audioplayer.screens.home.HomeUiState
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.SharingStarted
-import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.flow.stateIn
-import kotlinx.coroutines.flow.update
-import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class HomeViewModel @Inject constructor(
-  private val playbackManager: PlaybackManager
+  private val playbackManager: PlaybackManager,
 ) : ViewModel() {
 
   fun getExoPlayer() = playbackManager.player
