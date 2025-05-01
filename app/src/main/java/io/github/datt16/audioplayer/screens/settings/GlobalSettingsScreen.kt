22f0761f971da47d0d02c0d9076fcc33eca29ccd
datt16 <osatom.4639@gmail.com>
2025-05-02 01:15:20 +0900

    add cache list

--- app/src/main/java/io/github/datt16/audioplayer/screens/settings/GlobalSettingsScreen.kt
+++ app/src/main/java/io/github/datt16/audioplayer/screens/settings/GlobalSettingsScreen.kt
@@ -1,49 +1,155 @@
 package io.github.datt16.audioplayer.screens.settings
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.ElevatedButton
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.pulltorefresh.PullToRefreshBox
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import io.github.datt16.audioplayer.core.designsystem.AudioPlayerAppTheme
+import io.github.datt16.audioplayer.viewmodels.CacheItem
 import io.github.datt16.audioplayer.viewmodels.GlobalSettingsViewModel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun GlobalSettingsScreen(
   modifier: Modifier = Modifier,
   viewModel: GlobalSettingsViewModel = hiltViewModel(),
 ) {
+  val scope = rememberCoroutineScope()
+  var isRefreshing by remember { mutableStateOf(false) }
   val progress by viewModel.downloadProgress.collectAsState()
-  LazyColumn(modifier = modifier.fillMaxSize()) {
-    progress?.entries?.forEach { (key, value) ->
-      item(key) {
-        Row(modifier = Modifier.padding(horizontal = 16.dp)) {
-          Text(key)
-          Spacer(modifier = Modifier.weight(1f))
-          Column {
-            Text("${value.progress}%")
-            Text(value.state.name)
+  val cacheEntries by viewModel.cacheItems.collectAsState()
+
+  LaunchedEffect(Unit) {
+    viewModel.checkCacheEntries()
+  }
+
+  PullToRefreshBox(
+    modifier = modifier.fillMaxSize(),
+    isRefreshing = isRefreshing,
+    onRefresh = {
+      scope.launch {
+        isRefreshing = true
+        viewModel.checkCacheEntries()
+        delay(500)
+        isRefreshing = false
+      }
+    }
+  ) {
+    LazyColumn(modifier = Modifier.fillMaxSize(), verticalArrangement = Arrangement.Top) {
+      item {
+        Text(
+          text = "Download Progress",
+          style = AudioPlayerAppTheme.typography.titleMedium,
+          color = AudioPlayerAppTheme.colors.onSurface,
+          modifier = Modifier.padding(horizontal = 16.dp)
+        )
+      }
+      progress?.entries?.forEach { (key, value) ->
+        item(key) {
+          Row(modifier = Modifier.padding(horizontal = 16.dp)) {
+            Text(key)
+            Spacer(modifier = Modifier.weight(1f))
+            Column {
+              Text("${value.progress}%")
+              Text(value.state.name)
+            }
           }
         }
       }
-    }
-    item {
-      ElevatedButton(
-        onClick = { viewModel.startDownloadSample() },
-        modifier = Modifier
+      item {
+        ElevatedButton(
+          onClick = { viewModel.startDownloadSample() },
+          modifier = Modifier
             .fillMaxSize()
             .padding(horizontal = 16.dp)
-      ) {
-        Text("Download Sample")
+        ) {
+          Text("Download Sample")
+        }
+      }
+      item {
+        HorizontalDivider(
+          modifier = Modifier.padding(vertical = 16.dp)
+        )
+      }
+      item {
+        Text(
+          text = "Cached Files (${cacheEntries.size})",
+          style = AudioPlayerAppTheme.typography.titleMedium,
+          color = AudioPlayerAppTheme.colors.onSurface,
+          modifier = Modifier.padding(horizontal = 16.dp)
+        )
+      }
+      items(cacheEntries) {
+        CacheEntryItem(
+          it, modifier = Modifier
+            .padding(horizontal = 16.dp)
+            .padding(top = 8.dp)
+        )
       }
     }
+  }
+}
+
+@Composable
+private fun CacheEntryItem(cacheItem: CacheItem, modifier: Modifier = Modifier) {
+  Column(
+    modifier = modifier
+      .fillMaxWidth()
+      .height(80.dp)
+      .background(
+        color = AudioPlayerAppTheme.colors.surfaceVariant,
+        shape = RoundedCornerShape(8.dp)
+      )
+      .padding(16.dp)
+  ) {
+    Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceBetween) {
+      Text(
+        text = "key: ${cacheItem.key}",
+        style = AudioPlayerAppTheme.typography.labelMedium,
+        color = AudioPlayerAppTheme.colors.onSurface,
+        fontWeight = FontWeight.Bold,
+      )
+      Text(
+        text = "${cacheItem.sizeMb}MB",
+        style = AudioPlayerAppTheme.typography.labelMedium,
+        color = AudioPlayerAppTheme.colors.onSurface,
+      )
+    }
+    Text(
+      text = cacheItem.path,
+      style = AudioPlayerAppTheme.typography.labelMedium,
+      color = AudioPlayerAppTheme.colors.surfaceVariant,
+    )
   }
 }
--- app/src/main/java/io/github/datt16/audioplayer/viewmodels/GlobalSettingsViewModel.kt
+++ app/src/main/java/io/github/datt16/audioplayer/viewmodels/GlobalSettingsViewModel.kt
@@ -4,9 +4,12 @@ import androidx.annotation.OptIn
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import androidx.media3.common.util.UnstableApi
+import androidx.media3.datasource.cache.SimpleCache
 import dagger.hilt.android.lifecycle.HiltViewModel
 import io.github.datt16.audioplayer.core.player.download.DownloadController
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.stateIn
 import javax.inject.Inject
 
@@ -14,6 +17,7 @@ import javax.inject.Inject
 @HiltViewModel
 class GlobalSettingsViewModel @Inject constructor(
   private val downloadController: DownloadController,
+  private val simpleCache: SimpleCache,
 ) : ViewModel() {
   val downloadProgress = downloadController.getAllDownloadProgressMapFlow()
     .stateIn(
@@ -21,6 +25,9 @@ class GlobalSettingsViewModel @Inject constructor(
       started = SharingStarted.Lazily,
       initialValue = null,
     )
+
+  private val _cacheItems = MutableStateFlow<List<CacheItem>>(emptyList())
+  val cacheItems = _cacheItems.asStateFlow()
 
   fun startDownloadSample() {
     startDownload("sample01", "https://storage.googleapis.com/exoplayer-test-media-0/play.mp3")
@@ -37,4 +44,26 @@ class GlobalSettingsViewModel @Inject constructor(
   fun cancelDownload(contentId: String) {
     downloadController.cancelDownload(contentId)
   }
+
+  fun checkCacheEntries() {
+    val entries = simpleCache.keys.flatMap { key ->
+      simpleCache.getCachedSpans(key).mapNotNull { span ->
+        span.file?.let { file ->
+          CacheItem(
+            key = key,
+            path = file.absolutePath,
+            sizeMb = span.length.toDouble() / 1024 / 1024,
+          )
+        }
+      }
+    }
+
+    _cacheItems.value = entries
+  }
 }
+
+data class CacheItem(
+  val key: String,
+  val path: String,
+  val sizeMb: Double,
+)
